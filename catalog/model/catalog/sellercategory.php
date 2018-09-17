3da2eef8b95558954eae4fcd9eaed3ad9cb77f5c
AMWOLLO VICTOR <amwollo@yahoo.com>
2018-09-17 13:28:52 +0300

    Fix seller page showing Categories that have not been approved

--- catalog/model/catalog/sellercategory.php
+++ catalog/model/catalog/sellercategory.php
@@ -1,7 +1,8 @@
 <?php
 class ModelCatalogSellercategory extends Model {
+    
     public function getSellercategories($parent_id = 0,$seller_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' AND c.seller_id = '" . (int)$seller_id . "'ORDER BY c.sort_order, LCASE(cd.name)");
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND c.approve = 1 AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' AND c.seller_id = '" . (int)$seller_id . "'ORDER BY c.sort_order, LCASE(cd.name)");
 
 		return $query->rows;
 	}
